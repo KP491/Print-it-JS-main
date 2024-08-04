687f86a460e3fc02577e0409552f31d66e69ce7c
Karl Pohu <karlpohu8@gmail.com>
2024-08-04 15:48:59 +0200

    V.2

--- assets/script.js
+++ assets/script.js
@@ -17,42 +17,60 @@ const slides = [
 	}
 ]
 
-let slideIndex = 0;
-showSlides(slideIndex);
+const arrow_left = document.querySelector(".arrow_left")
+const arrow_right = document.querySelector(".arrow_right")
+
+arrow_left.addEventListener('click', () => {
+  console.log("Gauche")
+})
 
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+arrow_right.addEventListener('click', () => {
+  console.log("Droite")
+})
+const bannerImg = document.querySelector('.banner-img');
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.querySelectorAll("#banner ol li");
-  let dots = document.querySelectorAll(".dots .bullet");
 
-  if (n >= slides.length) { slideIndex = 0 }
-  if (n < 0) { slideIndex = slides.length - 1 }
+bannerImg.src = "./assets/images/slideshow/slide3.jpg"
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+// let slideIndex = 0;
+// showSlides(slideIndex);
 
-  slides[slideIndex].style.display = "block";
-  dots[slideIndex].className += " active";
-}
 
-document.querySelectorAll(".arrow_left").forEach(button => {
-  button.addEventListener("click", () => plusSlides(-1));
-});
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-document.querySelectorAll(".arrow_right").forEach(button => {
-  button.addEventListener("click", () => plusSlides(1));
-});
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   let i;
+//   let slides = document.querySelectorAll("#banner ol li");
+//   let dots = document.querySelectorAll(".dots .bullet");
+
+//   if (n >= slides.length) { slideIndex = 0 }
+//   if (n < 0) { slideIndex = slides.length - 1 }
+
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+
+//   slides[slideIndex].style.display = "block";
+//   dots[slideIndex].className += " active";
+// }
+
+// document.querySelectorAll(".arrow_left").forEach(button => {
+//   button.addEventListener("click", () => plusSlides(-1));
+// });
+
+// document.querySelectorAll(".arrow_right").forEach(button => {
+//   button.addEventListener("click", () => plusSlides(1));
+// });
