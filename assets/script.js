fd26f6dcd6a976c22d7d3eddf2179c4f2ddc3482
Karl Pohu <karlpohu8@gmail.com>
2024-08-29 10:25:54 +0200

    Commit final Fonctionnement

--- assets/script.js
+++ assets/script.js
@@ -30,10 +30,7 @@ arrow_left.addEventListener('click', () => {
   console.log(currentIndex)
   }
 
-  const bannerImg = document.querySelector('.banner-img');
-  bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image
-  
-  activeBulletPoint();
+  displaySlide();
 })
 
 
@@ -44,10 +41,8 @@ arrow_right.addEventListener('click', () => {
        currentIndex = 0
   console.log(currentIndex)
   }
-  const bannerImg = document.querySelector('.banner-img');
-  bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image
+  displaySlide();
   
-  activeBulletPoint();
 })
 const bannerImg = document.querySelector('.banner-img');
 
@@ -64,7 +59,22 @@ for(let i=0; i<4;i++){
   if(i===0){
     span.classList.add("dot_selected");
   }
+
+span.addEventListener('click', () => {
+
+  currentIndex = i;
+
+displaySlide();
+})
+
+
   dots.appendChild(span)
+}
+
+const displaySlide = () => {
+  const bannerImg = document.querySelector('.banner-img');
+  bannerImg.src = "./assets/images/slideshow/" + slides[currentIndex].image
+  activeBulletPoint();
 }
 
 const activeBulletPoint = () =>{
@@ -76,48 +86,3 @@ const activeBulletPoint = () =>{
 }
 
 
-// let slideIndex = 0;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.querySelectorAll("#banner ol li");
-//   let dots = document.querySelectorAll(".dots .bullet");
-
-//   if (n >= slides.length) { slideIndex = 0 }
-//   if (n < 0) { slideIndex = slides.length - 1 }
-
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-
-//   slides[slideIndex].style.display = "block";
-//   dots[slideIndex].className += " active";
-// }
-
-// document.querySelectorAll(".arrow_left").forEach(button => {
-//   button.addEventListener("click", () => plusSlides(-1));
-// });
-
-// document.querySelectorAll(".arrow_right").forEach(button => {
-//   button.addEventListener("click", () => plusSlides(1));
-// });
-
-// // Mettre à jour le texte de l'élément <p> avec la tagLine actuelle
-// const tagLine = slides[currentIndex].tagLine;
-// document.querySelector('p').textContent = tagLine; // Utilisation de textContent pour éviter de traiter le texte comme du HTML
-
-// // Afficher un message dans la console lors d'un clic sur une flèche, en indiquant la direction
-// console.log(`Clic sur la flèche ${direction}`);
